08e3b95e50c04de2e1f79f3a33183ff467d31a99
andyfan92 <txair921202@gmail.com>
2021-12-07 21:14:48 -0800

    Initial Commit

---
+++ Weather/Controller/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Weather
+//
+//  Created by fan on 10/28/21.
+//
+
+import Foundation
+
+//let apiKey = "WGXvLvr3mmG1bUwAfxAJfnZB5AUGMvGK"
+//let apiKey = "RVdi5NBIUK9GIdSTYUMGFSKg4GcppCpa"
+let apiKey = "0vppsRPxvlaudDDA2q3svscLiXee3cyk"
+let locationSearchURL = "https://dataservice.accuweather.com/locations/v1/cities/autocomplete?"
+let currentConditionURL = "https://dataservice.accuweather.com/currentconditions/v1/"
